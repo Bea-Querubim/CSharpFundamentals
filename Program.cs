e2b1e0c600363979deaaf3be6529fd0bcdb933f0
Bea Querubim <beatriz.querubim@take.net>
2023-05-05 11:20:04 -0300

    First commit - C# Fundamentals - Enums and Structs

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* instanciar uma struct,sem parametro
+         * 
+         * Product mouse= new Product();
+         * 
+         * se for sem parametro ele pula o metodo construtor!
+         * os valores mantem 0 ou \0 (para string)
+         */
+        //instanciar uma struct - opção com parametro
+
+        var mouse = new Product(1, "Mouse gamer", 300.90, EProductType.Product);
+        var manutencaoEletrica = new Product(2, "Manutenção elétrica residencial", 500, EProductType.Service);
+        // para sobreescrever ou alterar valores
+        mouse.Id = 55;
+
+        Console.WriteLine(mouse.Id);
+        Console.WriteLine(mouse.Name);
+        Console.WriteLine(mouse.Price);
+        Console.WriteLine(mouse.Type); //traz o valor em texto
+        Console.WriteLine((int)mouse.Type); //traz o valor em numeros
+
+    }
+}
+struct Product
+{
+    //metodo construtor
+    public Product(int id, string name, double price, EProductType type)// tem que conter o mesmo nome da struct
+    {
+        Id = id; // propriedade recebe parametro
+        Name = name; // propriedade recebe parametro 
+        Price = price; // propriedade recebe parametro
+        Type = type;  // type com minusculo é o parametro
+    }
+    public int Id;  //prioriedade
+    public string Name;  //prioriedade
+    public double Price;  //prioriedade
+    public EProductType Type; // EProductType -> é o tipo
+
+    public double PriceInDolar(double dolar)
+    {
+        return dolar * Price; //dolar -> parametro
+    }
+}
+
+//não pode estar dentro de uma classe - esta no mesmo nivel de estrutura!
+enum EProductType
+{
+    Product = 1,
+    Service = 2
+}
